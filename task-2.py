430b1787626937f4be81eefd04efe242d8e752b2
MasterHarpoon <115414810+MasterHarpoon@users.noreply.github.com>
2025-06-05 15:24:54 +0530

    Update task-2.py

--- task-2.py
+++ task-2.py
@@ -1,9 +1,10 @@
 # TASK 2
 
-num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+num_list = range(1,11)
 
-oneToFive = num_list[0:5]
+oneToFive = list(num_list[0:5])
 reverse = list(reversed(oneToFive))
-print("Original List: " + str(num_list))
+
+print("Original List: " + str(list(num_list)))
 print("Extracted first five elements: " + str(oneToFive))    
 print("Reversed extracted elements: " + str(reverse))  
